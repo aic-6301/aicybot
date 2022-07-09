94b8f4bd3071e572ffd6027db85339ccb5b1df2e
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-09 20:09:11 +0900

    天気機能追加
    そのうち改造するよ

--- main.py
+++ main.py
@@ -11,7 +11,50 @@ client = discord.Client()
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
-    print('ログインしたよ！')
+    print( client.user.name + 'でログインしたよ！')
+    @client.event
+# 天気
+# どこを取得するか
+citycodes = {
+    "土浦": '080020',
+    "水戸": '080010',
+    "札幌": '016010',
+    "仙台": '040010',
+    "東京": '130010',
+    "横浜": '140010',
+    "名古屋": '230010',
+    "大阪": '270000',
+    "広島": '340010',
+    "福岡": '400010',
+    "鹿児島": '460010',
+    "那覇": '471010'
+}
+
+ # 取得
+    
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+
+    reg_res = re.compile(u"(.+)の天気").search(message.content)
+    if reg_res:
+
+      if reg_res.group(1) in citycodes.keys():
+
+        citycode = citycodes[reg_res.group(1)]
+        resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
+        resp = json.loads(resp.decode('utf-8'))
+# メッセージにして送信
+        msg = resp['location']['city']
+        msg += "の天気は、\n"
+        for f in resp['forecasts']:
+          msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+        msg += "です。"
+
+        await client.send_message(message.channel, message.author.mention + msg)
+
+      else:
+        await client.send_message(message.channel, message.content + "は対応していないか受信ができませんでした。")
 
 # メッセージ受信時に動作する処理
 @client.event
