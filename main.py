d1c23bf760d3358385608840b6f03faba2c804d9
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-09 14:42:53 +0900

    返信メッセージ変更

--- main.py
+++ main.py
@@ -20,8 +20,17 @@ async def on_message(message):
     if message.author.bot:
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if message.content == 'おはよう':
+        await message.channel.send('おはようございます！')
 
+# 334への返信
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「334」と発言したら「な阪関無」が返る処理
+    if message.content == '334':
+        await message.channel.send('な阪関無')
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
