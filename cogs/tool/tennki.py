ffa04251b6e7ba013fae2c04d44f3c51ca5561db
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-10 11:12:38 +0900

    Update tennki.py

--- cogs/tool/tennki.py
+++ cogs/tool/tennki.py
@@ -6,9 +6,9 @@ import asyncio
 
 # 読み込んだら通知
 @commands.Cog.listener()
-   # Cogが読み込まれた時に発動
-    async def on_ready(self):
-        print('天気cogが読み込まれたよ！')
+  # Cogが読み込まれた時に発動
+ async def on_ready(self):
+  print('天気cogが読み込まれたよ！')
 # 天気
 # どこを取得するか
 citycodes = {
