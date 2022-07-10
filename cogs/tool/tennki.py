9f80681ffddb35ff4b3d72fd4d69455ae3d412f6
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-10 11:09:13 +0900

    空白調整

--- cogs/tool/tennki.py
+++ cogs/tool/tennki.py
@@ -5,10 +5,10 @@ import traceback
 import asyncio
 
 # 読み込んだら通知
- @commands.Cog.listener()
-     # Cogが読み込まれた時に発動
-     async def on_ready(self):
-         print('天気cogが読み込まれたよ！')
+@commands.Cog.listener()
+   # Cogが読み込まれた時に発動
+    async def on_ready(self):
+        print('天気cogが読み込まれたよ！')
 # 天気
 # どこを取得するか
 citycodes = {
