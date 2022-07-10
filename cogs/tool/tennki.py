bc118c53723560fa665cf0ad1a01854fa3bdfe41
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-10 10:43:43 +0900

    なんかエラー

--- cogs/tool/tennki.py
+++ cogs/tool/tennki.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 
 # 読み込んだら通知
- @commands.Cog.listener()
+ #@commands.Cog.listener()
      # Cogが読み込まれた時に発動
-     async def on_ready(self):
-         print('天気cogが読み込まれたよ！')
+    # async def on_ready(self):
+        # print('天気cogが読み込まれたよ！')
 # 天気
 # どこを取得するか
 citycodes = {
