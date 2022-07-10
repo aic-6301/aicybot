7143222c28bbbe8886ccce3cd5d29de48bdc5168
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-07-10 13:58:16 +0900

    Update bot.py

--- cogs/bot/bot.py
+++ cogs/bot/bot.py
@@ -12,13 +12,8 @@ import asyncio
     embed.add_field(name="help",value=f"このコマンドです。\nbotのコマンドについて書かれています、",inline=False)
     embed.add_field(name="serverinfo",value=f"サーバー情報が取得できます。",inline=False)
     embed.add_field(name="tenki",value="天気が取得できます",inline=False)
-    #embed.add_field(name="",value=guild.owner,inline=False)
-    #embed.add_field(name="チャンネル数",value=f"{len(text_channels)}",inline=False)
-    #embed.add_field(name="ロール数",value=f"{len(roles)}",inline=False)
-    #embed.add_field(name="サーバーブースト数",value=guild.premium_subscription_count,inline=False)
-    #embed.add_field(name="メンバー数",value=guild.member_count,inline=False)
     embed.set_footer(text=f"実行者：{ctx.author} ",icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
   
-  def setup(bot):
-  bot.add_cog(help(bot))
+def setup(bot):
+ bot.add_cog(help(bot))
